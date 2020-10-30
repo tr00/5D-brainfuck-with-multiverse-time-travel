bb48b20112d8f69e8b79d2d3385ebf8c5661eee8
tr00 <tryyyy@gmx.de>
2020-10-30 11:24:58 +0100

    final tweaks & stricter typing

--- interpreter.jl
+++ interpreter.jl
@@ -1,17 +1,6 @@
 struct EOF <: Exception end
 
-"""
-    This isnt really needed for an 1-char per instruction
-    language but it allows me to abstract the functionality
-    of each instruction into its own function.
-    Its also super useful to avoid rescanning the source code
-    for each and every bracket / parenthesis (including that [(])([)] )
-"""
-struct Token
-    rep::Function
-    val::UInt32
-    Token(rep::Function, val::UInt32 = zero(UInt32)) = new(rep, val)
-end
+const _one = one(UInt32)
 
 """
     Simple datastruct that uses a dictionary as tape with a potentially multiple cell pointers.
@@ -21,10 +10,10 @@ end
 """
 mutable struct Timeline
     tape::Dict{Int, UInt8}
-    memory_pointers::Vector{Int}
+    memory_pointers::Vector{UInt32}
     instruction_pointer::Int
     history::Vector{Dict{Int, UInt8}}
-    Timeline(tape::Dict{Int, UInt8} = Dict(1 => 0x0), mems = Int[1], instr = 1, history = []) = new(tape, mems, instr, history)
+    Timeline(tape::Dict{Int, UInt8} = Dict(1 => 0x0), mems = UInt32[_one], instr = 1, history = []) = new(tape, mems, instr, history)
 end
 
 function error(msg::AbstractString)
@@ -41,19 +30,26 @@ function assert()
     if !endswith(ARGS[1], ".5dbfwmtt") && !endswith(ARGS[1], ".bf")
         printstyled("warning: source file is not a .5dbfwmtt file", color=:red)
     end
-    open(f -> [read(f, String)...], ARGS[1], "r")
+    open(ARGS[1], "r") do f
+        res = Char[]
+        while !eof(f)
+            c = read(f, Char)::Char
+            c ∈ "><+-.,[]()~@^v" && push!(res, c)
+        end
+        res
+    end  # O( l )
 end
 
 """
-    Create a snapshot of the current tape
+    Create a minimal snapshot of all the cells currently pointed to
 """
 snapshot(tl::Timeline) = push!(tl.history, Dict([cell => tl.tape[cell] for cell in tl.memory_pointers]))
 
 """
     Move all memory pointers in this timeline 1 cell to the right.
 """
-function move(tl, tl_pointer, timelines, token)
-    tl.memory_pointers .+= 1
+function move(tl::Timeline, ::Int, ::Vector{Timeline})
+    tl.memory_pointers .+= _one
     # initializing cells to zero (if they have no value)
     get!.(Ref(tl.tape), tl.memory_pointers, 0x0)
 end
@@ -62,43 +58,40 @@ end
     Move all memory pointers in this timeline 1 cell to the left.
     (note that bf interpreters usually start pointing at the leftmost cell)
 """
-function back(tl, tl_pointer, timelines, token)
-    tl.memory_pointers .-= 1
+function back(tl::Timeline, ::Int, ::Vector{Timeline})
+    any([m === _one for m in tl.memory_pointers]) && error("out of bounds")
+    tl.memory_pointers .-= _one
     get!.(Ref(tl.tape), tl.memory_pointers, 0x0)
 end
 
 """
     Increment all cells pointed to in this timeline.
 """
-function incr(tl, tl_pointer, timelines, token)
-    for cell in tl.memory_pointers
-        snapshot(tl)
-        tl.tape[cell] += 0x1
-    end
+function incr(tl::Timeline, ::Int, ::Vector{Timeline})
+    snapshot(tl)
+    (cell -> tl.tape[cell] += 0x1).(tl.memory_pointers)
 end
 
 """
     Decrement all cells pointed to in this timeline.
 """
-function decr(tl, tl_pointer, timelines, token)
-    for cell in tl.memory_pointers
-        snapshot(tl)
-        tl.tape[cell] -= 0x1
-    end
+function decr(tl::Timeline, ::Int, ::Vector{Timeline})
+    snapshot(tl)
+    (cell -> tl.tape[cell] -= 0x1).(tl.memory_pointers)
 end
 
 """
     Output a character for all cells pointed to in this timeline.
     (using ascii encoding)
 """
-function putc(tl, tl_pointer, timelines, token)
-    print.(Char.(getindex.(Ref(tl.tape), tl.memory_pointers)::Vector{UInt8}))
+function putc(tl::Timeline, ::Int, ::Vector{Timeline})
+    print.(Char.(getindex.(Ref(tl.tape), tl.memory_pointers)))
 end
 
 """
     Input a character and store it in all cells pointed to in this timeline.
 """
-function getc(tl, tl_pointer, timelines, token)
+function getc(tl::Timeline, ::Int, ::Vector{Timeline})
     snapshot(tl)
     setindex!.(Ref(tl.tape), read(stdin, UInt8), tl.memory_pointers)
 end
@@ -106,25 +99,25 @@ end
 """
     Move this instruction pointer to the matching ] if all cells pointed to in this timeline are 0.
 """
-function loop(tl, tl_pointer, timelines, token)
+function loop(tl::Timeline, val::UInt32)
     if all([tl.tape[cell] == 0 for cell in tl.memory_pointers])
-        tl.instruction_pointer = token.val # massive speed gain right here
+        tl.instruction_pointer = val # massive speed gain right here
     end
 end
 
 """
     Move this instruction pointer back to the matching [ if any cells pointed to in this timeline are nonzero.
 """
-function goto(tl, tl_pointer, timelines, token) 
+function goto(tl::Timeline, val::UInt32) 
     if any([tl.tape[cell] ≠ 0 for cell ∈ tl.memory_pointers])
-        tl.instruction_pointer = token.val
+        tl.instruction_pointer = val
     end
 end
 
 """
     Rewind the current tape back in time by 1 step.
 """
-function time(tl, tl_pointer, timelines, token)
+function time(tl::Timeline, ::Int, ::Vector{Timeline})
     length(tl.history) > 0 && merge!(tl.tape, pop!(tl.history))
 end
 
@@ -134,10 +127,10 @@ end
     Spawn a new instruction pointer within the newly spawned timeline,
     beginning execution immediately after this instruction.
 """
-function copy(tl, tl_pointer, timelines, token)
+function copy(tl::Timeline, tl_pointer::Int, timelines::Vector{Timeline}, val::UInt32)
     ntl = deepcopy(tl)
     ntl.instruction_pointer += 1
-    tl.instruction_pointer = token.val # jump to the matching )
+    tl.instruction_pointer = val # jump to the matching )
     insert!(timelines, tl_pointer + 1, ntl)
 end
 
@@ -146,7 +139,7 @@ end
     kill this timeline and all the memory/instruction pointers currently in it.
     Otherwise, do nothing.
 """
-function kill(tl, tl_pointer, timelines, token)
+function kill(::Timeline, tl_pointer::Int, timelines::Vector{Timeline})
     if tl_pointer ≠ 1
         popat!(timelines, tl_pointer)
     end
@@ -155,23 +148,24 @@ end
 """
     Move all memory pointers in this timeline to the same location in the next ("lower") parallel universe.
 """
-function next(tl, tl_pointer, timelines, token)
+function next(tl::Timeline, tl_pointer::Int, timelines::Vector{Timeline})
     if tl_pointer ≠ length(timelines)
         append!(timelines[tl_pointer + 1].memory_pointers, tl.memory_pointers)
         get!.(Ref(timelines[tl_pointer + 1].tape), tl.memory_pointers, 0x0)
     end
-    tl.memory_pointers = Int[]
+    tl.memory_pointers = UInt32[]
 end
 
 """
     Move all memory pointers in this timeline to the same location in the previous ("higher") parallel universe.
 """
-function prev(tl, tl_pointer, timelines, token)
+function prev(tl::Timeline, tl_pointer::Int, timelines::Vector{Timeline})
     if tl_pointer ≠ 1
         append!(timelines[tl_pointer - 1].memory_pointers, tl.memory_pointers)
+        # defaulting new cells to 0
         get!.(Ref(timelines[tl_pointer - 1].tape), tl.memory_pointers, 0x0)
     end
-    tl.memory_pointers = Int[]
+    tl.memory_pointers = UInt32[]
 end
 
 """
@@ -179,7 +173,7 @@ end
     freeze this program pointer for the duration of this step.
     Otherwise, continue forward.
 """
-function wait(tl, tl_pointer, timelines, token)
+function wait(tl::Timeline, tl_pointer::Int, timelines::Vector{Timeline})
     # has a lower tl & that tl has mem pointers
     if length(timelines) > tl_pointer && length(timelines[tl_pointer + 1].memory_pointers) > 0
         tl.instruction_pointer -= 1
@@ -187,15 +181,10 @@ function wait(tl, tl_pointer, timelines, token)
 end
 
 """
-    Do nothing
-"""
-noop(_...) = nothing # one of the coolest functions ive ever written
-
-"""
-    Throw an EOF exception and thus end this program if the current timeline is
+    Ends this program if the current timeline is
     the main timeline. Otherwise it terminates the timeline
 """
-eof(tl, tl_pointer, timelines, token) = tl_pointer == 1 ? throw(EOF()) : popat!(timelines, tl_pointer)
+done(::Timeline, tl_pointer::Int, timelines::Vector{Timeline}) = tl_pointer == 1 ? exit(0) : popat!(timelines, tl_pointer)
 
 """
     Converts all chars into their respective token.
@@ -206,70 +195,56 @@ function lexer(src::Vector{Char})
     # vanilla bf
     d = Dict('>'=>move,'<'=>back,'+'=>incr,'-'=>decr,'.'=>putc,','=>getc,'['=>loop,']'=>goto,
     # 5D + time travel extension
-    '~'=>time,'('=>copy,')'=>kill,'v'=>next,'^'=>prev, '@'=>wait)
-    R1 = Char[]
-    k = one(UInt32)
+    '~'=>time,'('=>copy,')'=>kill,'v'=>next,'^'=>prev,'@'=>wait)
+    res = Function[]
+    k = _one
+    loops = UInt32[]
     while k ≤ length(src)
         p = src[k]
-        if p === '#'
-            k = UInt32(something(findnext(isequal('\n'), src, k), length(src)))
-        elseif p ∈ "><+-.,~v^@()[]"
-            push!(R1, p)
-        end
-        k += one(UInt32)
-    end
-    res = Token[]
-    k = one(UInt32)
-    loops = UInt32[]
-    while k ≤ length(R1)
-        p = R1[k]
         if p ∈ "><+-.,~v^@)"
-            push!(res, Token(d[p]))
+            push!(res, d[p])
         elseif p === '['
             n = 1
-            f = zero(UInt32)
+            f = _one
             while n > 0
-                f + k ≥ length(R1) && error("expected closing bracket")
-                n += (R1[(f+=one(UInt32)) + k] === '[') - (R1[f + k] === ']')
+                f + k > length(src) && error("expected closing bracket")
+                n += (src[f + k] === '[') - (src[f + k] === ']')
+                f += _one
             end
             push!(loops, k)
-            push!(res, Token(loop, f+k))
+            v = f + k - _one
+            push!(res, (tl::Timeline, ::Int, ::Vector{Timeline}) -> loop(tl, v))
         elseif p === ']'
             isempty(loops) && error("unexpected closing bracket")
-            push!(res, Token(goto, pop!(loops)))
+            v = pop!(loops)
+            push!(res, (tl::Timeline, ::Int, ::Vector{Timeline}) -> goto(tl, v))
         elseif p === '('
             n = 1
-            f = zero(UInt32)
+            f = _one
             while n > 0
-                f + k ≥ length(R1) && error("expected closing parenthesis")
-                n += (R1[(f+=one(UInt32)) + k] === '(') - (R1[f + k] === ')')
+                f + k > length(src) && error("expected closing parenthesis")
+                n += (src[f + k] === '(') - (src[f + k] === ')')
+                f += _one
             end
-            push!(res, Token(copy, f+k))
+            v = f + k - _one
+            push!(res, (tl::Timeline, tl_pointer::Int, timelines::Vector{Timeline}) -> copy(tl, tl_pointer, timelines, v))
         end
-        k += one(UInt32)
+        k += _one
     end
-    return push!(res, Token(eof))
+    return push!(res, done)
 end
 
-function interpret(_src::Vector{Token})
+function interpret(src::Vector{Function}) # worst: O( i d m ) average: O( i d ) best: O( i )
     timelines = Timeline[Timeline()]
-    # its just one global and this one is typeconst
-    global src = _src::Vector{Token}
-    while true # program ends when the main timeline throws EOF()
+    # O( i ) ; i = total instructions
+    while true # program ends when the main timeline calls done()
         tl_pointer = 1
+        # O( d ) ; d = depths of timelines
         while tl_pointer ≤ length(timelines)
             tl = timelines[tl_pointer]
-            token = src[tl.instruction_pointer]
-            try
-                (token.rep)(tl, tl_pointer, timelines, token)
-            catch e
-                if e isa EOF
-                    println()
-                    return 
-                else
-                    rethrow(e)
-                end
-            end
+            # all of these functions have either constant time O( 1 )
+            # or vectorized & efficient O( m ) ; m = amount of memory pointers 
+            src[tl.instruction_pointer](tl, tl_pointer, timelines)
             tl_pointer += 1
             tl.instruction_pointer += 1
         end
